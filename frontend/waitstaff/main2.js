be5e2bee0567353820049a9db09e4864bd2abaa6
mokdog13 <moku.kau13@gmail.com>
2018-04-02 14:38:06 -0500

    This has working JS to HTML for waitstaff

--- frontend/waitstaff/main2.js
+++ frontend/waitstaff/main2.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	
 
 	$('#test_button').click(function(){
-		test();
+		getName();
 	});
 });
 
@@ -16,15 +16,28 @@ var url='http://www.johnknowlesportfolio.com:443';
 function getName(){
 	$.get('http://johnknowlesportfolio.com:443/api/parties',function(data){
 		console.log(data);
-		let output= '<div class="card">'//<ul class= "list-group">';
+		//for(i=1;i<5;i++){
+		var i=0;
 		$.each(data, function(key,party){
-			console.log(data);
+			i++;
+			let output2= '<p  class="card-text"> people';
+			let output= '<p class="card-text"> name';
+			output += '<div class="card">';
 			output += '<div id="name_put" class="card-body">' + party.p_Name + '</div>';//<li class="list-group-item">';
+			output += '</div>'
+			output+='</p>';
+			output2 += '<li class="list-group-item d-flex justify-content-between align-items-center"> number of people';
+			output2 += '<span class="badge badge-primary badge-pill">'+party.p_Count+'</span>'
+			output2 += '</li>'
+			output2 += '</p>'
+			$('#table_'+i).html(output);
+			$('#c_'+i).html(output2);
+			console.log(output);
+			console.log(output2);
 			//output += party.p_Name + '<span class = "party_name"></span>';
 			//output += '<div class="pull-right"> <a class="btn btn-success btn-view-task" data-task-name="'+print.p_Name+'" data-task-id="'+print._id+'">View</a> <a class="btn btn-primary btn-edit-task" data-task-name="'+print.p_Name+'" data-task-id="'+print._id+'">Edit</a> <a class="btn btn-danger btn-delete-task" data-task-id="'+print._id+'">Delete</a></div>';
 		});
-		output+='</div>';
-		$('#parties').html(output);
+	//}
 	}); 
 }
 //Get data from a server
