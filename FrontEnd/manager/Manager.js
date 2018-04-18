ac2c1d9e6d551eadec3ddb62f3f8bee7b028cba6
Hoang Huynh <hhuynh2011@gmail.com>
2018-04-17 21:57:40 -0500

    fdsafsafsfsfda

--- FrontEnd/manager/Manager.js
+++ FrontEnd/manager/Manager.js
@@ -4,54 +4,23 @@ function check(){
 		$.each(data, function(key,party)
 		{
 			var j= party.p_Table-1000;
-			
-			if(party.p_WaiterID == 1000)
-			{
-				if(party.p_hasOrdered)
-					document.getElementById("Table"+j).style.backgroundColor = "red";			
-				else if(party.p_NeedsRefill)
-					document.getElementById("Table"+j).style.backgroundColor = "orange";
-				else if(party.p_HasPaid)
-					document.getElementById("Table"+j).style.backgroundColor = "white";
-				else if(party.p_isSeated)
-					document.getElementById("Table"+j).style.backgroundColor = "green";	
-			}
+			var table=0;
 
-			else if(party.p_WaiterID == 1001)
-			{
-				if(party.p_hasOrdered)
-					document.getElementById("Table"+j).style.backgroundColor = "red";			
-				else if(party.p_NeedsRefill)	
-					document.getElementById("Table"+j).style.backgroundColor = "orange";
-				else if(party.p_HasPaid)
-					document.getElementById("Table"+j).style.backgroundColor = "white";
-				else if(party.p_isSeated)
-					document.getElementById("Table"+j).style.backgroundColor = "green";									
-			}
-
-			else if(party.p_WaiterID == 1002)
-			{
-				if(party.p_hasOrdered)
-					document.getElementById("Table"+j).style.backgroundColor = "red";			
-				else if(party.p_NeedsRefill)	
-					document.getElementById("Table"+j).style.backgroundColor = "orange";
-				else if(party.p_HasPaid)
-					document.getElementById("Table"+j).style.backgroundColor = "white";
-				else if(party.p_isSeated)
-					document.getElementById("Table"+j).style.backgroundColor = "green";									
-			}
-
-			else if(party.p_WaiterID == 1003)
-			{
-				if(party.p_hasOrdered)
-					document.getElementById("Table"+j).style.backgroundColor = "red";			
-				else if(party.p_NeedsRefill)
-					document.getElementById("Table"+j).style.backgroundColor = "orange";
-				else if(party.p_HasPaid)
-					document.getElementById("Table"+j).style.backgroundColor = "white";
-				else if(party.p_isSeated)
-					document.getElementById("Table"+j).style.backgroundColor = "green";									
-			}
+			for(j = 1001; j < 1016; j++)
+				{
+					table = j-1000;
+					if(j == party.p_Table)
+					{
+						if(party.p_hasOrdered)
+							document.getElementById("Table"+table).style.backgroundColor = "red";			
+						else if(party.p_NeedsRefill)
+							document.getElementById("Table"+table).style.backgroundColor = "orange";
+						else if(party.p_HasPaid)
+							document.getElementById("Table"+table).style.backgroundColor = "white";
+						else if(party.p_isSeated)
+							document.getElementById("Table"+table).style.backgroundColor = "green";	
+					}
+				}
 		});
 	}); 
 	setTimeout(check,5000);
