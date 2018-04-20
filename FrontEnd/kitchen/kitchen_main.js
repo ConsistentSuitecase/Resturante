54505503a0a2a647d3b186a50aaa9814a92045ae
TI\Tuan <tuanmai204@gmail.com>
2018-04-19 20:14:57 -0500

    no message

--- FrontEnd/kitchen/kitchen_main.js
+++ FrontEnd/kitchen/kitchen_main.js
@@ -6,14 +6,6 @@ $(document).ready(function(){
 
 function getOrders()
 {
-<<<<<<< HEAD
-	//console.log("working");
-	var active_orders=[];
-	$.get('http://localhost:443/api/parties',function(data){
-			$.each(data, function(key,party){
-				console.log(party.p_Orders);
-				//active_orders = party.p_Orders;
-=======
 	var active_orders=[];
 	$.get('http://localhost:443/api/parties',function(data){
 		$.each(data, function(key,party){
@@ -29,128 +21,111 @@ function getOrders()
 				}
 			});
 
-		});
-	});
-<<<<<<< HEAD
-<<<<<<< HEAD
-	//console.log(active_orders);
-	// var i = 0;
-	// $.each(active_orders.order, function(key,order)
-	// {
-	// 	i++;
-	// 	console.log(orders);
-	// 	let output = '<div class ="card" id="body"';
-	// 	output += '<p>'+active_orders[i] +'</p>';
-	// 	output += '</div>';
-	// 	$('#cardb'+1).html(output);
 
-	// });
-=======
+});
+	});
 
 	//console.log(party.p_Orders);
 
 
->>>>>>> 1964640f07eb4dc870b5c4608e8e2023a4733a2f
 
 
-=======
->>>>>>> d840214228e2cb2faa3fdf6992fb83c7bf24b32e
 }
 
 function clearScreen_1()
 {
-	document.getElementById("cardb1").innerHTML = "";
+	document.getElementById("1001").innerHTML = "";
 
 }
 
 function clearScreen_2()
 {
-	document.getElementById("cardb2").innerHTML = "";
+	document.getElementById("1002").innerHTML = "";
 
 }
 
 function clearScreen_3()
 {
-	document.getElementById("cardb3").innerHTML = "";
+	document.getElementById("1003").innerHTML = "";
 
 }
 
 function clearScreen_4()
 {
-	document.getElementById("cardb4").innerHTML = "";
+	document.getElementById("1004").innerHTML = "";
 
 }
 
 function clearScreen_5()
 {
-	document.getElementById("cardb5").innerHTML = "";
+	document.getElementById("1005").innerHTML = "";
 
 }
 
 function clearScreen_6()
 {
-	document.getElementById("cardb6").innerHTML = "";
+	document.getElementById("1006").innerHTML = "";
 
 }
 
 function clearScreen_7()
 {
-	document.getElementById("cardb7").innerHTML = "";
+	document.getElementById("1007").innerHTML = "";
 
 }
 
 function clearScreen_8()
 {
-	document.getElementById("cardb8").innerHTML = "";
+	document.getElementById("1008").innerHTML = "";
 
 }
 
 function clearScreen_9()
 {
-	document.getElementById("cardb9").innerHTML = "";
+	document.getElementById("1009").innerHTML = "";
 
 }
 
 function clearScreen_10()
 {
-	document.getElementById("cardb10").innerHTML = "";
+	document.getElementById("1010").innerHTML = "";
 
 }
 
 
 function clearScreen_11()
 {
-	document.getElementById("cardb11").innerHTML = "";
+	document.getElementById("1011").innerHTML = "";
 
 }
 
 function clearScreen_12()
 {
-	document.getElementById("cardb12").innerHTML = "";
+	document.getElementById("1012").innerHTML = "";
 
 }
 
 function clearScreen_13()
 {
-	document.getElementById("cardb13").innerHTML = "";
+	document.getElementById("1013").innerHTML = "";
 
 }
 
 function clearScreen_14()
 {
-	document.getElementById("cardb14").innerHTML = "";
+	document.getElementById("1014").innerHTML = "";
 
 }
 
 function clearScreen_15()
 {
-	document.getElementById("cardb15").innerHTML = "";
+	document.getElementById("1015").innerHTML = "";
 
 }
 
 function clearScreen_16()
 {
-	document.getElementById("cardb16").innerHTML = "";
+	document.getElementById("1016").innerHTML = "";
 
 }
 
