7b75d0e8de9452705fce5a9fcf951b42779207ce
Hoang Huynh <hhuynh2011@gmail.com>
2018-04-21 18:17:05 -0500

    Manager/Report update: Report tracks time & date

--- FrontEnd/manager/Manager.js
+++ FrontEnd/manager/Manager.js
@@ -17,10 +17,12 @@ function check(){
 							document.getElementById("Table"+y).style.backgroundColor = "red";			
 						else if(party.p_NeedsRefill)
 							document.getElementById("Table"+y).style.backgroundColor = "orange";
-						else if(party.p_HasPaid)
-							document.getElementById("Table"+y).style.backgroundColor = "white";
 						else if(party.p_isSeated)
 							document.getElementById("Table"+y).style.backgroundColor = "green";	
+						else if(party.p_HasPaid)
+							document.getElementById("Table"+y).style.backgroundColor = "white";		
+						else
+							document.getElementById("Table"+y).style.backgroundColor = "white";													
 					}
 				}
 
@@ -36,10 +38,18 @@ function displayOrderedFood()
 	var chicken = 0;
 	var lettuce = 0;
 	var cake = 0;
+	var coke = 0;
+	var tea = 0;
+	var water = 0;
+
 	var salmonT = 0;
 	var chickenT = 0;
 	var lettuceT = 0;
 	var cakeT = 0;	
+	var cokeT = 0;
+	var teaT = 0;
+	var waterT = 0;
+
 	$.get(url+'/api/parties',function(data)
 	{
 		$.each(data, function(key,party)
@@ -74,11 +84,26 @@ function displayOrderedFood()
 						cake++;
 						cakeT+=party.p_Orders[i].itemPrice;
 					}
+					else if(party.p_Orders[i].itemName=="coke")
+					{
+						coke++;
+						cokeT+=party.p_Orders[i].itemPrice;
+					}
+					else if(party.p_Orders[i].itemName=="tea")
+					{
+						tea++;
+						teaT+=party.p_Orders[i].itemPrice;
+					}
+					else if(party.p_Orders[i].itemName=="water")
+					{
+						water++;
+						waterT+=party.p_Orders[i].itemPrice;
+					}										
 					totalCost+=party.p_Orders[i].itemPrice;
 				}
-				document.getElementById("quantity").innerHTML = "Quantity<br>"+salmon+"<br>"+chicken+"<br>"+lettuce+"<br>"+cake+"<br><br><br><p align='right'>Total:</p>";
-				document.getElementById("moneh").innerHTML = "Food total<br>$"+salmonT+".00<br>$"+chickenT+".00<br>$"+lettuceT+".00<br>$"+cakeT+".00<br>";
-				document.getElementById("moneh").innerHTML += "<br><br>$"+totalCost+".00<br>";
+				document.getElementById("quantity").innerHTML = "Quantity<br>"+salmon+"<br>"+chicken+"<br>"+lettuce+"<br>"+cake+"<br>"+coke+"<br>"+tea+"<br>"+water+"<br><br><br><p align='right'>Total:</p>";
+				document.getElementById("moneh").innerHTML = "Food total<br>$"+salmonT.toFixed(2)+"<br>$"+chickenT.toFixed(2)+"<br>$"+lettuceT.toFixed(2)+"<br>$"+cakeT.toFixed(2)+"<br>$"+cokeT.toFixed(2)+"<br>$"+teaT.toFixed(2)+"<br>$"+waterT.toFixed(2)+"<br>";
+				document.getElementById("moneh").innerHTML += "<br><br>$"+totalCost.toFixed(2)+"<br>";
 //				output+='</ul>'
 				//$('#sentOrders').html(output);
 				return;
@@ -86,4 +111,47 @@ function displayOrderedFood()
 		});
 	});
 }
+
+
+function dates() {
+var d = new Date();
+var minute=d.getMinutes();
+var hour=d.getHours();
+var month=d.getMonth()+1;
+
+var apm = "AM";
+
+if(hour > 12)
+{
+	hour -= 12;
+	apm="PM";
+}
+if(minute < 10)
+	minute="0"+minute;
+else if(hour == 00);
+//---------------------------------------------------------------------------------
+    switch(d.getDay())
+    {
+      case 0:
+        document.getElementById("DateHTML").innerHTML = "Sunday";
+      case 1:
+        document.getElementById("DateHTML").innerHTML = "Monday";
+      case 2:
+        document.getElementById("DateHTML").innerHTML = "Tuesday";
+      case 3:
+        document.getElementById("DateHTML").innerHTML = "Wednesday";
+      case 4:
+        document.getElementById("DateHTML").innerHTML = "Thursday";
+      case 5:
+        document.getElementById("DateHTML").innerHTML = "Friday";
+      case 6:
+        document.getElementById("DateHTML").innerHTML = "Saturday";
+      default:
+        break;
+
+    }
+//------------switch case is all you need for getting the day----------------------
+    document.getElementById("DateHTML").innerHTML += ", "+month+"/"+d.getDate()+" ("+hour+":"+minute+apm+")";
+
+} 
 	//output+='<p id="list">'+party.p_Orders[i].itemName+'   $'+party.p_Orders[i].itemPrice+'</p>';
